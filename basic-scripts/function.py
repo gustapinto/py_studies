1a76157a1fbc1b9a706845112cb47f61cc888224
ghp2201 <ghp2201@gmail.com>
2020-05-11 13:01:26 -0300

    Simple function creation (def) example

---
+++ basic-scripts/function.py
@@ -0,0 +1,11 @@
+#some simple function example
+
+#declaring the function with def
+def plus(a, b):
+    return a + b;
+
+number1 = int(input())
+number2 = int(input())
+
+print(plus(number1, number2))
+
