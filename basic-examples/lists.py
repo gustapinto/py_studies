3649fcb9d95e0d4a01860ddd6ba3b72c156ab968
Gustavo Pinto <gustavo.pinto@cassum.dev>
2020-07-16 17:00:32 -0300

    Create a basic example for lists

---
+++ basic-examples/lists.py
@@ -0,0 +1,18 @@
+#some examples of operations with lists
+
+list = ['cléber', 'clébinho', 'clébson', 'cléber']
+
+print(list.count('cléber'))
+
+print(list.index('clébinho'))
+
+list.reverse()
+print(list)
+
+list.append('clébão')
+print(list)
+
+list.sort()
+print(list)
+
+print(list.pop())
