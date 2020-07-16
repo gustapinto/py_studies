2fca5662a25a4aba32fdfd1f3a7a02430ba34f99
Gustavo Pinto <gustavo.pinto@cassum.dev>
2020-07-16 16:33:07 -0300

    Improve hello world example

--- basic-examples/helloWorld.py
+++ basic-examples/helloWorld.py
@@ -1,1 +1,8 @@
-print('hello world')
+text1 = 'Hello'
+text2 = 'World'
+
+print('hello world') #the simple way
+
+print('{} {}'.format(text1, text2)) #the ordered way
+
+print('{text1} {text2}'.format(text2=text2, text1=text1)) #the named way
