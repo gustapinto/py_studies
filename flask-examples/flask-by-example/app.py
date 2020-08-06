122b38351c5322a84f2a74c3da6aebbc0e57002c
ghp2201 <ghp2201@gmail.com>
2020-08-06 11:25:02 -0300

    Create a multiple enviroments config
    Create a config to handle mutiple enviroments, like Production, Staging
    and Development.

--- flask-examples/flask-by-example/app.py
+++ flask-examples/flask-by-example/app.py
@@ -1,10 +1,17 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
     return "Hello World"
+
+@app.route('/<name>')
+def hello_name(name):
+    return f"Hello {name}!"
 
 if __name__ == '__main__':
     app.run()
