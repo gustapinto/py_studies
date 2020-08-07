97920101a0937f197db53d508f0e83fa3a9d0e33
ghp2201 <gustavo.pinto@cassum.dev>
2020-08-07 14:33:52 -0300

    Implement redis on project

--- flask-examples/flask-by-example/app.py
+++ flask-examples/flask-by-example/app.py
@@ -4,67 +4,96 @@ import operator
 import re
 import nltk
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
-from models import Result
+q = Queue(connection=conn)
+
+from models import *
+
+def count_and_save_words(url):
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append('Unable to get URL.')
+        return {"Error": errors}
+
+    raw = BeautifulSoup(r.text, 'html.parser').get_text()
+
+    nltk.data.path.append('./nltk_data/')
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+
+    nonPunct = re.compile('.*[A-Za-z]*.')
+
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_words_count = Counter(raw_words)
+
+    no_stop_words = [w for w in raw_words if w.lower not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    try:
+        result = Result(url=url,
+                        result_all=raw_words_count,
+                        result_no_stop_words=no_stop_words_count)
+
+        db.session.add(result)
+        db.session.commit()
+
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"Error": errors}
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
     results = {}
 
     if request.method == "POST":
-        try:
-            url = request.form['url']
-            r = requests.get(url)
+        from app import count_and_save_words
 
-            print(r.text)
-        except:
-            errors.append('Unable to get URL.')
-            return render_template('index.html', errors=errors)
+        url = request.form['url']
 
-        # Text processing
-        if r:
-            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+        if not url[:8].startswith(('https://', 'http://')):
+            url = 'http://' + url
 
-            nltk.data.path.append('./nltk_data/')
-            tokens = nltk.word_tokenize(raw)
-            text = nltk.Text(tokens)
+        job = q.enqueue_call(func=count_and_save_words,
+                             args=(url,),
+                             result_ttl=5000)
 
-            nonPunct = re.compile('.*[A-Za-z]*.')
+        print(job.get_id())
 
-            raw_words = [w for w in text if nonPunct.match(w)]
-            raw_words_count = Counter(raw_words)
+    return render_template('index.html', results=results)
 
-            no_stop_words = [w for w in raw_words if w.lower not in stops]
-            no_stop_words_count = Counter(no_stop_words)
 
-            results = sorted(no_stop_words_count.items(),
-                             key=operator.itemgetter(1),
-                             reverse=True)
+@app.route('/results/<job_key>', methods=['GET'])
+def get_results(job_key):
+    job = Job.fetch(job_key, connection=conn)
 
-            try:
-                result = Result(url=url,
-                                result_all=raw_words_count,
-                                result_no_stop_words=no_stop_words_count)
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(result.result_no_stop_words.items(),
+                         key=operator.itemgetter(1),
+                         reverse=True)
 
-                db.session.add(result)
-                db.session.commit()
-            except:
-                errors.append("Unable to add item to database.")
-
-    return render_template('index.html', errors=errors, results=results)
+        return jsonify(results)
+    else:
+        'Nay', 202
 
 
 if __name__ == '__main__':
