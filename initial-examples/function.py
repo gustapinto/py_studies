c78d5a90000fe48ae3e064693901b2dbe25bff5d
ghp2201 <ghp2201@gmail.com>
2020-10-07 08:49:09 -0300

    Improved the functions example

--- initial-examples/function.py
+++ initial-examples/function.py
@@ -1,11 +1,8 @@
-''' Some function implementation examples'''
+""" Some function implementation examples """
 
 # Declare a function with def
 def plus(n1, n2):
     return n1 + n2
-
-print(f'Ordered sum = {plus(2, 3)}')  # Call function w/ordered parameters
-print(f'Named sum = {plus(n2=6, n1=7.5)}')  # Call function w/named parameters
 
 # Create a first class function
 # The first class function returns another function
@@ -31,3 +28,32 @@ compare_names(name2='Josias')
 # Lambda functions can also be inserted into variables
 lambda_in_a_var = lambda n1, n2: print(f'Lambda in a var sum = {n1 + n2}')
 lambda_in_a_var(4, 5)
+
+""" Dealing with arguments """
+
+# Python functions can deal with positional or named arguments
+print(f'Ordered sum = {plus(2, 3)}')  # Call function w/positional parameters
+print(f'Named sum = {plus(n2=6, n1=7.5)}')  # Call function w/named parameters
+
+# The positional only and keyword only arguments can be marked with a '/,'
+# and '*,' respectively on the function definition
+def combined_function(positional_only, /, positional_or_keyword,
+                      *, keyword_only):
+    return print(f'{positional_only}, {positional_or_keyword}, {keyword_only}')
+
+combined_function(11, 12, keyword_only=13)  # Or...
+combined_function(11, positional_or_keyword=12, keyword_only=13)
+
+# We can also define functions that can take a variable number of arguments
+def args_function(*args):
+    # The positional arguments are defined with *, with the name *args being
+    # just a convention
+    return print(args)  # The arguments are treated like a tuple.
+
+def kwargs_function(**kwargs):
+    # The named arguments are defined with **, with the name **kwargs being
+    # just a convention
+    return print(kwargs)  # The arguments are treated like a dictionary
+
+args_function(1, 2, 3)
+kwargs_function(key1=1, key2=2, key3=3)
